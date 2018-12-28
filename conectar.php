3a1e363575d8ca55294ea9b732f51cde6a9b8958
AlessandraBardalesandra <46227579+AlessandraBardalesandra@users.noreply.github.com>
2018-12-28 15:53:24 -0500

    Update conectar.php
    actualizando datos para la conección con azure

--- conectar.php
+++ conectar.php
@@ -1,5 +1,5 @@
 <?php
-$conectar = mysqli_connect('localhost','nycol','123456','cecin');
+$conectar = mysqli_connect('cecinweb-mysqldbserver.mysql.database.azure.com','mysqldbuser@cecinweb-mysqldbserver','Sistema$1','cecin');
 
 if(mysqli_connect_errno($conectar)){
 	echo "Conexion fallida". mysqli_connect_error();
